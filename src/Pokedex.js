bd51997427684418f29d29204be2c35b1860b57b
Nathália <nathaliaveneziano@icloud.com>
2021-02-23 19:26:51 -0300

    Exercicio Estado e Eventos React

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
     render() {
         return (
             <div className="pokedex">
